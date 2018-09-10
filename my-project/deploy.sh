92e13c90eb075878ba758af5da1074fba4e7d8fc
fishbn <fishbonebeats@gmail.com>
2018-09-10 20:46:22 +0300

    fist commit

---
+++ my-project/deploy.sh
@@ -0,0 +1,3 @@
+git add .
+git commit =am "Deploy to HEROKU"
+git push heroku master
---
+++ my-project/server/index.js
@@ -0,0 +1,4 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import path from 'path'
+import history from 'connect-history-api-fallback'
